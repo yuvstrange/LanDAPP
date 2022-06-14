53984c931a65682870b79f069b8f1fd79b670d96
unknown <myuvraj701@gmail.com>
2022-06-14 20:52:26 +0530

    first commit

---
+++ Server/backend/Config/db_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  email: "myuvraj701@gmail.com",
+  password: "Yuvinotsooff1221@",
+  NEXMO_API_KEY: "3cb07eb7",
+  NEXMO_API_SECRET: "9vkOhlYqjT6NFcfu",
+  NEXMO_FROM_NUMBER: "9560477241",
+  MongoURI: 'mongodb+srv://yuvdont:fD6_GxpN5Fuk.b5
+  @cluster0.uva9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
