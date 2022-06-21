55900ef9c6869cb0807cc705284edcbf6fc94063
yuvstrange <57724812+yuvstrange@users.noreply.github.com>
2022-06-21 12:10:18 +0530

    Update db_config.js

--- Server/backend/Config/db_config.js
+++ Server/backend/Config/db_config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  email: "myuvraj701@gmail.com",
-  password: "Yuvinotsooff1221@",
-  NEXMO_API_KEY: "3cb07eb7",
-  NEXMO_API_SECRET: "9vkOhlYqjT6NFcfu",
-  NEXMO_FROM_NUMBER: "9560477241",
-  MongoURI: 'mongodb+srv://yuvdont:fD6_GxpN5Fuk.b5
-  @cluster0.uva9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  email: "",
+  password: "",
+  NEXMO_API_KEY: "",
+  NEXMO_API_SECRET: "",
+  NEXMO_FROM_NUMBER: "",
+  MongoURI: ''
 }
